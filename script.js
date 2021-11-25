4d95b1ed0319f22f1875b1681b2ac4ef6ab0491f
Psyh2409 <psyh2409@gmail.com>
2021-11-25 21:18:52 +0200

    init commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.body = document.createElement('body')
 let div = document.createElement('div');
 document.body.style.padding = '10vw';
@@ -5,7 +6,28 @@ div.style.background = 'blue';
 div.style.height = '60vh';
 div.setAttribute('class', 'outer');
 let header = document.createElement('header');
-header.style.width = '100%'
+header.style.width = '100%';
+div.append(document.createElement('hr'))
+document.body.append(div);
+let outer = document.querySelector('div.outer')
+let input1 = document.createElement('input');
+input1.type = 'number';
+let val1 = input1.value;
+div.append(input1);
+div.append(document.createElement('br'));
+div.append(document.createElement('br'));
+let input2 = document.createElement('input');
+input2.type = 'number';
+let val2 = input2.value;
+div.append(input2);
+
+div.style.textAlign = 'center';
+let result = document.createElement('h1');
+add = (x,y) => {result.innerText = parseInt(x+y)};
+sub = (x,y) => {result.innerText = parseInt(x-y)};
+mult = (x,y) => {result.innerText = parseInt(x*y)};
+divis = (x,y) => {result.innerText = parseInt(x/y)};
+
 for(let i = 0; i<4; i++){
     let button = document.createElement('button');
     button.style.width = '10%'
@@ -13,19 +35,19 @@ for(let i = 0; i<4; i++){
     switch (i) {
         case 0:
             button.textContent = '+';
-            button.onclick = (x,y) => {x+y};
+            button.onclick = add(val1, val2);
             break;
     case 1:
         button.textContent = '-';
-        button.onclick = (x,y) => {x-y};
+        button.onclick = sub(val1, val2);
             break;
     case 2:
         button.textContent = '*';
-    button.onclick = (x,y) => {x*y};
+    button.onclick = mult(val1, val2);
     break;
     case 3:
         button.textContent = '/';
-        button.onclick = (x,y) => {x/y};
+        button.onclick = divis(val1, val2);
           break;
     }
     header.append(button);
@@ -34,8 +56,7 @@ header.style.display = 'flex';
 header.style.flexDirection = 'row';
 header.style.justifyContent = 'space-around';
 header.style.verticalAlign = 'middle';
-div.append(header);
-div.append(document.createElement('hr'))
-// let body = document.body.innerHTML;
-document.body.append(div);
+div.append(result)
+outer.append(header);
+
 
