a947ec12910754f3b312a3934a4ad0cc15d7a8f4
Psyh2409 <psyh2409@gmail.com>
2021-11-25 22:52:00 +0200

    Fuck yea!!!

--- script.js
+++ script.js
@@ -3,55 +3,81 @@ document.body = document.createElement('body')
 let div = document.createElement('div');
 document.body.style.padding = '10vw';
 div.style.background = 'blue';
-div.style.height = '60vh';
+div.style.height = '30vh';
 div.setAttribute('class', 'outer');
 let header = document.createElement('header');
 header.style.width = '100%';
-div.append(document.createElement('hr'))
 document.body.append(div);
 let outer = document.querySelector('div.outer')
 let input1 = document.createElement('input');
+
+let val1, val2;
+outer.append(header);
+div.append(document.createElement('hr'))
+input1.setAttribute('id', 'in1');
 input1.type = 'number';
-let val1 = input1.value;
+console.log(val1);
 div.append(input1);
 div.append(document.createElement('br'));
 div.append(document.createElement('br'));
 let input2 = document.createElement('input');
+input2.setAttribute('id', 'in2');
 input2.type = 'number';
-let val2 = input2.value;
+
+console.log(val2);
 div.append(input2);
 div.append(document.createElement('br'));
 div.append(document.createElement('br'));
 div.style.textAlign = 'center';
 let h1 = document.createElement('h1');
 let result = 0;
-h1.textContent = result;
-add = (x,y) => {result = x+y};
-sub = (x,y) => {result = x-y};
-mult = (x,y) => {result = x*y};
-divis = (x,y) => {result = x/y};
+div.append(h1)
+add = () => {
+    val1 = document.getElementById('in1').value;
+    val2 = document.getElementById('in2').value;
+    result = parseInt(val1) + parseInt(val2);
+    h1.textContent = result;
+};
+sub = () => {
+    val1 = document.getElementById('in1').value;
+    val2 = document.getElementById('in2').value;
+    result = parseInt(val1) - parseInt(val2);
+    h1.textContent = result;
+};
+mult = () => {
+    val1 = document.getElementById('in1').value;
+    val2 = document.getElementById('in2').value;
+    result = parseInt(val1) * parseInt(val2);
+    h1.textContent = result;
+};
+divis = () => {
+    val1 = document.getElementById('in1').value;
+    val2 = document.getElementById('in2').value;
+    result = parseInt(val1) / parseInt(val2);
+    h1.textContent = result;
+};
 
-for(let i = 0; i<4; i++){
+for (let i = 0; i < 4; i++) {
     let button = document.createElement('button');
     button.style.width = '10%'
-    button.style.fontSize ='5vw'
+    button.style.fontSize = '5vw'
     switch (i) {
         case 0:
             button.textContent = '+';
-            button.onclick = add(val1, val2);
+            button.setAttribute('onclick', 'add()');
             break;
-    case 1:
-        button.textContent = '-';
-        button.onclick = sub(val1, val2);
+        case 1:
+            button.textContent = '-';
+            button.setAttribute('onclick', 'sub()');
             break;
-    case 2:
-        button.textContent = '*';
-    button.onclick = mult(val1, val2);
-    break;
-    case 3:
-        button.textContent = '/';
-        button.onclick = divis(val1, val2);
-          break;
+        case 2:
+            button.textContent = '*';
+            button.setAttribute('onclick', 'mult()');
+            break;
+        case 3:
+            button.textContent = '/';
+            button.setAttribute('onclick', 'divis()');
+            break;
     }
     header.append(button);
 }
@@ -59,7 +85,8 @@ header.style.display = 'flex';
 header.style.flexDirection = 'row';
 header.style.justifyContent = 'space-around';
 header.style.verticalAlign = 'middle';
-div.append(result)
-outer.append(header);
+
+
+
 
 
