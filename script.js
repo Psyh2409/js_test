29f8b043761b45673193b8f981e58a0c16a92366
Psyh2409 <psyh2409@gmail.com>
2021-11-25 23:24:22 +0200

    style.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.body = document.createElement('body')
 let div = document.createElement('div');
 document.body.style.padding = '10vw';
 div.style.background = 'blue';
-div.style.height = '30vh';
+div.style.height = '50vh';
 div.setAttribute('class', 'outer');
 let header = document.createElement('header');
 header.style.width = '100%';
@@ -16,18 +16,18 @@ outer.append(header);
 div.append(document.createElement('hr'))
 input1.setAttribute('id', 'in1');
 input1.type = 'number';
-console.log(val1);
+input1.style.fontSize = '2vw';
 div.append(input1);
 div.append(document.createElement('br'));
 div.append(document.createElement('br'));
 let input2 = document.createElement('input');
 input2.setAttribute('id', 'in2');
 input2.type = 'number';
-
+input2.style.fontSize = '2vw';
 console.log(val2);
 div.append(input2);
-div.append(document.createElement('br'));
-div.append(document.createElement('br'));
+// div.append(document.createElement('br'));
+// div.append(document.createElement('br'));
 div.style.textAlign = 'center';
 let h1 = document.createElement('h1');
 let result = 0;
@@ -61,6 +61,7 @@ for (let i = 0; i < 4; i++) {
     let button = document.createElement('button');
     button.style.width = '10%'
     button.style.fontSize = '5vw'
+    button.style.margin = '5vh'
     switch (i) {
         case 0:
             button.textContent = '+';
@@ -81,10 +82,12 @@ for (let i = 0; i < 4; i++) {
     }
     header.append(button);
 }
+
 header.style.display = 'flex';
 header.style.flexDirection = 'row';
 header.style.justifyContent = 'space-around';
 header.style.verticalAlign = 'middle';
+
 
 
 
